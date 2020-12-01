9dd29f08b97425c841ce5b29c6616e2ad58c506a
Zack Swafford <zdfs1121@gmail.com>
2020-11-30 22:33:18 -0800

    Add soln for 12/01/20

---
+++ 2020/01/sln.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from itertools import combinations
+
+
+with open('inp.txt') as fin:
+    ls = [int(e.strip()) for e in fin.readlines()]
+
+
+# part 1
+for e, f in combinations(ls, 2):
+    if e + f == 2020:
+        print(e * f)
+        break
+
+
+# part 2
+for e, f, g in combinations(ls, 3):
+    if e + f + g == 2020:
+        print(e * f * g)
+        break
