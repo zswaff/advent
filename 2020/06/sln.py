e1efde3c8d6f3738384e4d3768608b98d398ee52
Zack Swafford <zdfs1121@gmail.com>
2020-12-05 21:36:08 -0800

    Add a 20/6

---
+++ 2020/06/sln.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+with open('inp.txt') as fin:
+    groups = [
+        [set(f) for f in e.split('\n')]
+        for e in fin.read().strip().split('\n\n')
+    ]
+
+
+# part 1
+print(sum(len(e[0].union(*e[1:])) for e in groups))
+
+
+# part 2
+print(sum(len(e[0].intersection(*e[1:])) for e in groups))
