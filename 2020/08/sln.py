f2e02b7195947505b2f8dcc178bc360b419526eb
Zack Swafford <zdfs1121@gmail.com>
2020-12-07 21:22:45 -0800

    Add soln for 20-8

--- 2020/08/sln.py
+++ 2020/08/sln.py
@@ -7,7 +7,39 @@ with open('inp.txt') as fin:
 
 
 # part 1
-
+def run(instrs):
+    acc = 0
+    idx = 0
+    visited = set()
+    finished = False
+    while True:
+        if idx in visited:
+            break
+        if idx == len(instrs):
+            finished = True
+            break
+        visited.add(idx)
+        cmd, amt = instrs[idx].split(' ')
+        if cmd == 'nop':
+            idx += 1
+            continue
+        if cmd == 'acc':
+            acc += int(amt)
+            idx += 1
+            continue
+        idx += int(amt)
+    return acc, finished
+print(run(lines)[0])
 
 
 # part 2
+for i, l in enumerate(lines):
+    if l.startswith('acc'):
+        continue
+    if l.startswith('nop'):
+        nl = 'jmp' + l[3:]
+    else:
+        nl = 'nop' + l[3:]
+    a, f = run(lines[:i] + [nl] + lines[i+1:])
+    if f:
+        print(a)
