20ae709e63f1e91047cf25d5ed5547e0ecfc4b53
Zack Swafford <zdfs1121@gmail.com>
2020-12-12 10:43:37 -0800

    Add soln for 20-12

--- 2020/12/sln.py
+++ 2020/12/sln.py
@@ -2,13 +2,83 @@
 # -*- coding: utf-8 -*-
 
 
+DIRS = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+
+
 with open('inp.txt') as fin:
-    lines = [e.strip() for e in fin.readlines()]
+    instrs = [
+        (f[0], int(f[1:])) for f in
+        [e.strip() for e in fin.readlines()]
+    ]
 
 
 # part 1
+x = y = d = 0
+for cmd, amt in instrs:
+    if cmd == 'N':
+        y += amt
+        continue
+    if cmd == 'S':
+        y -= amt
+        continue
+    if cmd == 'E':
+        x += amt
+        continue
+    if cmd == 'W':
+        x -= amt
+        continue
+    if cmd == 'L':
+        d = (d - (amt // 90)) % 4
+        continue
+    if cmd == 'R':
+        d = (d + (amt // 90)) % 4
+        continue
+    if cmd == 'F':
+        xm, ym = DIRS[d]
+        x += xm * amt
+        y += ym * amt
+        continue
 
+print(abs(x) + abs(y))
 
 
 # part 2
+x = y = 0
+wx, wy = 10, 1
+for cmd, amt in instrs:
+    if cmd == 'N':
+        wy += amt
+        continue
+    if cmd == 'S':
+        wy -= amt
+        continue
+    if cmd == 'E':
+        wx += amt
+        continue
+    if cmd == 'W':
+        wx -= amt
+        continue
+    if cmd == 'L':
+        r = (amt // 90) % 4
+        if r == 1:
+            wx, wy = -wy, wx
+        if r == 2:
+            wx, wy = -wx, -wy
+        if r == 3:
+            wx, wy = wy, -wx
+        continue
+    if cmd == 'R':
+        r = (amt // 90) % 4
+        if r == 1:
+            wx, wy = wy, -wx
+        if r == 2:
+            wx, wy = -wx, -wy
+        if r == 3:
+            wx, wy = -wy, wx
+        continue
+    if cmd == 'F':
+        x += wx * amt
+        y += wy * amt
+        continue
 
+print(abs(x) + abs(y))
