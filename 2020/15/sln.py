7a9c9d67d12cfbce0697b6ce0573a1a179e05982
Zack Swafford <zdfs1121@gmail.com>
2020-12-14 22:12:30 -0800

    Add soln for 20-15

--- 2020/15/sln.py
+++ 2020/15/sln.py
@@ -2,13 +2,32 @@
 # -*- coding: utf-8 -*-
 
 
-with open('inp.txt') as fin:
-    lines = [e.strip() for e in fin.readlines()]
+from collections import defaultdict
+
+
+INP = [15,12,0,14,3,1]
 
 
 # part 1
-
+r = list(reversed(INP))
+for i in range(2020 - len(r)):
+    try:
+        f = r[1:].index(r[0]) + 1
+    except ValueError:
+        f = 0
+    r.insert(0, f)
+print(r[0])
 
 
 # part 2
-
+r = INP[-1]
+d = defaultdict(list)
+for i, e in enumerate(INP):
+    d[e].append(i)
+for i in range(len(INP), 30000000):
+    if len(d[r]) > 1:
+        r = i - d[r][-2] - 1
+    else:
+        r = 0
+    d[r].append(i)
+print(r)
