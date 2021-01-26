7482f2721fadc9dcd3b3d70a9852f1aea30ce7fd
Zack Swafford <zdfs1121@gmail.com>
2021-01-26 13:22:54 -0800

    Add soln for 17-01 and 17-02

--- 2017/01/sln.py
+++ 2017/01/sln.py
@@ -3,12 +3,12 @@
 
 
 with open('inp.txt') as fin:
-    lines = [e.strip() for e in fin.readlines()]
+    ns = [int(e) for e in fin.read().strip()]
 
 
 # part 1
-
+print(sum(e for i, e in enumerate(ns) if e == ns[(i + 1) % len(ns)]))
 
 
 # part 2
-
+print(sum(e for i, e in enumerate(ns) if e == ns[(i + len(ns) // 2) % len(ns)]))
---
+++ 2017/03/sln.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+INP = 289326
+
+
+# part 1
+
+
+
+# part 2
+
