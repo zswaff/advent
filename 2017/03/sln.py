0be0b431798e31bee413db61243b380232a4da1d
Zack Swafford <zdfs1121@gmail.com>
2021-02-27 15:02:49 -0800

    Add soln for 17-03 and 17-04

--- 2017/03/sln.py
+++ 2017/03/sln.py
@@ -2,12 +2,36 @@
 # -*- coding: utf-8 -*-
 
 
+from itertools import count
+
+
 INP = 289326
 
 
 # part 1
-
+c, i, n = INP - 1, 0, 0
+for i in count(1):
+    n = 8 * i
+    if c > n:
+        c -= n
+    else:
+        break
+print(i + abs(((c - 1) % (i * 2)) - i + 1))
 
 
 # part 2
-
+def solve():
+    g = {(0, 0): 1}
+    x, y = 1, -1
+    for i in count(2, 2):
+        for dx, dy in [(0, 1), (-1, 0), (0, -1), (1, 0)]:
+            for _ in range(i):
+                x, y = x + dx, y + dy
+                g[(x, y)] = sum(
+                    g.get((x + ax, y + ay), 0)
+                    for ax in range(-1, 2) for ay in range(-1, 2)
+                )
+                if g[(x, y)] > INP:
+                    return g[(x, y)]
+        x, y = x + 1, y - 1
+print(solve())
---
+++ 2017/04/sln.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from collections import Counter
+
+
+with open('inp.txt') as fin:
+    lines = [e.strip() for e in fin.readlines()]
+
+
+# part 1
+print(sum(Counter(l.split(' ')).most_common()[0][1] == 1 for l in lines))
+
+
+# part 2
+print(sum(
+    Counter(''.join(sorted(e)) for e in l.split(' ')).most_common()[0][1] == 1
+    for l in lines
+))
