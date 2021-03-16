aa599da5141766b2c9269463634f2114868eb188
Zack Swafford <zdfs1121@gmail.com>
2021-03-16 16:06:02 -0700

    Add soln for 17-08 and 17-09

---
+++ 2017/08/sln.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from math import inf
+from collections import defaultdict
+
+with open('inp.txt') as fin:
+    lines = [e.strip() for e in fin.readlines()]
+
+# part 1
+d = defaultdict(int)
+for line in lines:
+    uk, iod, amt, _, ck, cop, cv = line.split(' ')
+    if not eval(f'{d[ck]} {cop} {cv}'):
+        continue
+    d[uk] += int(amt) * (1 if iod == 'inc' else -1)
+print(max(d.values()))
+
+# part 2
+m = -inf
+d = defaultdict(int)
+for line in lines:
+    uk, iod, amt, _, ck, cop, cv = line.split(' ')
+    if not eval(f'{d[ck]} {cop} {cv}'):
+        continue
+    d[uk] += int(amt) * (1 if iod == 'inc' else -1)
+    m = max(m, d[uk])
+print(m)
---
+++ 2017/09/sln.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+with open('inp.txt') as fin:
+    inp = fin.read().strip()
+
+# part 1
+total = 0
+level = 0
+skip = False
+garbage = False
+for c in inp:
+    if skip:
+        skip = False
+        continue
+    if c == '!':
+        skip = True
+        continue
+    if garbage:
+        if c == '>':
+            garbage = False
+        continue
+    if c == '<':
+        garbage = True
+        continue
+    if c == '}':
+        total += level
+        level -= 1
+        continue
+    if c == '{':
+        level += 1
+        continue
+print(total)
+
+# part 2
+total = 0
+skip = False
+garbage = False
+for c in inp:
+    if skip:
+        skip = False
+        continue
+    if c == '!':
+        skip = True
+        continue
+    if garbage:
+        if c == '>':
+            garbage = False
+        else:
+            total += 1
+        continue
+    if c == '<':
+        garbage = True
+        continue
+print(total)
