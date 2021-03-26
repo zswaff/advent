87b9e7f4a16c8b53333998f87dc8299eb4987037
Zack Swafford <zdfs1121@gmail.com>
2021-03-26 15:59:44 -0700

    Add soln for 17-13

---
+++ 2017/13/sln.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from itertools import count
+
+
+with open('inp.txt') as fin:
+    lines = [e.strip() for e in fin.readlines()]
+scns = {}
+for l in lines:
+    k, v = [int(e) for e in l.split(': ')]
+    scns[k] = v, v * 2 - 2
+
+
+# part 1
+print(sum(k * v for k, (v, r) in scns.items() if k % r == 0))
+
+
+# part 2
+for i in count():
+    if all((i + k) % r != 0 for k, (_, r) in scns.items()):
+        print(i)
+        break
