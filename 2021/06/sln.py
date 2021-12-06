c74a7129433b4177fcfdf275e5b4a38167331730
Zack Swafford <zdfs1121@gmail.com>
2021-12-06 00:50:54 -0800

    Solve 21-06

--- 2021/06/sln.py
+++ 2021/06/sln.py
@@ -7,8 +7,20 @@ from web import *
 
 
 # part 1
-
+c = Counter(int(e) for e in dt.split(','))
+for _ in range(80):
+    nc = Counter({k - 1: v for k, v in c.items() if k > 0})
+    nc[6] += c[0]
+    nc[8] = c[0]
+    c = nc
+sm(sum(c.values()))
 
 
 # part 2
-
+c = Counter(int(e) for e in dt.split(','))
+for _ in range(256):
+    nc = Counter({k - 1: v for k, v in c.items() if k > 0})
+    nc[6] += c[0]
+    nc[8] = c[0]
+    c = nc
+sm(sum(c.values()))
