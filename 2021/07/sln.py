cffdf4639c9249759fa457a3240def4c93d96772
Zack Swafford <zdfs1121@gmail.com>
2021-12-06 22:07:45 -0800

    Solve 21-07

--- 2021/07/sln.py
+++ 2021/07/sln.py
@@ -6,9 +6,17 @@ from common import *
 from web import *
 
 
-# part 1
+ns = [int(e) for e in dt.split(',')]
+mx = max(ns)
 
+
+# part 1
+sm(min(sum(abs(f - e) for f in ns) for e in range(0, mx + 1)))
 
 
 # part 2
+def t(n):
+    return (n * (n + 1)) // 2
 
+
+sm(min(sum(t(abs(f - e)) for f in ns) for e in range(0, mx + 1)))
