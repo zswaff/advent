a34c40d5b63ce2d20f79aff0bcd4e4a3a4fc0082
Zack Swafford <zdfs1121@gmail.com>
2021-12-10 14:54:57 -0800

    Solve 21-10

--- 2021/10/sln.py
+++ 2021/10/sln.py
@@ -6,9 +6,41 @@ from common import *
 from web import *
 
 
-# part 1
+PAIRS = {'(': ')', '[': ']', '{': '}', '<': '>'}
 
+
+# part 1
+scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
+c = 0
+for l in ls:
+    s = []
+    for e in l:
+        if e not in scores:
+            s.append(e)
+            continue
+        f = s.pop()
+        if e != PAIRS[f]:
+            c += scores[e]
+            break
+sm(c)
 
 
 # part 2
-
+scores = {')': 1, ']': 2, '}': 3, '>': 4}
+cs = []
+for l in ls:
+    s = []
+    for e in l:
+        if e not in scores:
+            s.append(e)
+            continue
+        f = s.pop()
+        if e != PAIRS[f]:
+            break
+    else:
+        c = 0
+        for e in reversed(s):
+            c *= 5
+            c += scores[PAIRS[e]]
+        cs.append(c)
+sm(sorted(cs)[len(cs) // 2])
