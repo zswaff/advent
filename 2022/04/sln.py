3d78b3bc493d2d4bb6f8e16ccca1110b197e13ee
Zack Swafford <zack@zswaff.com>
2022-12-03 21:12:25 -0800

    Solve 2022-12-03

--- 2022/04/sln.py
+++ 2022/04/sln.py
@@ -6,8 +6,22 @@ from common import *
 
 
 # part 1
-
+c = 0
+for l in ls:
+    n1, x1, n2, x2 = pa(l, "{i}-{i},{i}-{i}")
+    r1 = set(range(n1, x1 + 1))
+    r2 = set(range(n2, x2 + 1))
+    if (r1 <= r2) or (r2 <= r1):
+        c += 1
+sm(c)
 
 
 # part 2
-
+c = 0
+for l in ls:
+    n1, x1, n2, x2 = pa(l, "{i}-{i},{i}-{i}")
+    r1 = set(range(n1, x1 + 1))
+    r2 = set(range(n2, x2 + 1))
+    if len(r1 & r2) > 0:
+        c += 1
+sm(c)
