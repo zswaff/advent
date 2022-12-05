2b6492eb7e9414113bd470a97a5366fda7c187ae
Zack Swafford <zack@zswaff.com>
2022-12-04 21:19:59 -0800

    Solve 2022-12-04

--- 2022/05/sln.py
+++ 2022/05/sln.py
@@ -4,10 +4,37 @@
 
 from common import *
 
-
 # part 1
-
+stacks, ops = ss
+stacks = reversed(stacks[:-1])
+ps = defaultdict(list)
+for l in stacks:
+    lp = list(l[1::4])
+    for i, c in enumerate(lp, 1):
+        if c == " ":
+            continue
+        ps[i].append(c)
+for l in ops:
+    n, s, d = pa(l, "move {i} from {i} to {i}")
+    mv = ps[s][-n:]
+    ps[s] = ps[s][:-n]
+    ps[d] += reversed(mv)
+sm("".join(ps[i][-1] for i in range(1, len(ps) + 1)))
 
 
 # part 2
-
+stacks, ops = ss
+stacks = reversed(stacks[:-1])
+ps = defaultdict(list)
+for l in stacks:
+    lp = list(l[1::4])
+    for i, c in enumerate(lp, 1):
+        if c == " ":
+            continue
+        ps[i].append(c)
+for l in ops:
+    n, s, d = pa(l, "move {i} from {i} to {i}")
+    mv = ps[s][-n:]
+    ps[s] = ps[s][:-n]
+    ps[d] += mv
+sm("".join(ps[i][-1] for i in range(1, len(ps) + 1)))
