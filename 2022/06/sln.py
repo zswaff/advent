c85d8267b515045727da4508735cd2e82e815df9
Zack Swafford <zack@zswaff.com>
2022-12-05 23:31:11 -0800

    Solve 2022-12-05

---
+++ 2022/06/sln.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from common import *
+
+
+# part 1
+l = ls[0]
+sm(next(i for i in range(4, len(l) + 1) if len(set(list(l[i - 4 : i]))) == 4))
+
+
+# part 2
+sm(next(i for i in range(14, len(l) + 1) if len(set(list(l[i - 14 : i]))) == 14))
