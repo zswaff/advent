809d86cf9aa59aa7ab4a4d121682b40bdd678051
Zack Swafford <zack@zswaff.com>
2022-12-06 22:56:32 -0800

    Solve 2022-12-06

--- 2022/07/sln.py
+++ 2022/07/sln.py
@@ -5,7 +5,58 @@
 from common import *
 
 
+root = {}
+root[".."] = root
+curr = root
+for s in dt[2:].split("\n$ "):
+    ls = s.split("\n")
+    cmd = ls[0]
+    if cmd.startswith("cd"):
+        dest = cmd[3:]
+        if dest == "/":
+            curr = root
+            continue
+        curr = curr[dest]
+        continue
+    for l in ls[1:]:
+        t, n = l.split(" ")
+        if t == "dir":
+            curr[n] = {}
+            curr[n][".."] = curr
+            continue
+        curr[n] = int(t)
+
+
 # part 1
+smalls = []
+
+
+def rec(n):
+    if isinstance(n, int):
+        return n
+    sz = sum(rec(v) for k, v in n.items() if k != "..")
+    if sz < 100000:
+        smalls.append(sz)
+    return sz
+
+
+rec(root)
+sm(sum(smalls))
 
 
 # part 2
+szs = []
+
+
+def rec(n):
+    if isinstance(n, int):
+        return n
+    sz = sum(rec(v) for k, v in n.items() if k != "..")
+    szs.append(sz)
+    return sz
+
+
+tot = rec(root)
+szs.sort()
+mn = 30000000 - (70000000 - tot)
+sm(next(e for e in szs if e >= mn))
