e7984523dbbfc953422de3d4ae274d127093e5e1
Zack Swafford <zack@zswaff.com>
2022-12-07 21:18:02 -0800

    Solve 2022-12-07

--- 2022/08/sln.py
+++ 2022/08/sln.py
@@ -5,9 +5,31 @@
 from common import *
 
 
-# part 1
+g = gr(ls, int)
+mx = max(x for x, _ in g.keys())
+my = max(y for _, y in g.keys())
 
+
+# part 1
+visible = set()
+for (x, y), e in g.items():
+    if (
+        all(g[(x, i)] < e for i in range(0, y))
+        or all(g[(x, i)] < e for i in range(y + 1, my + 1))
+        or all(g[(i, y)] < e for i in range(0, x))
+        or all(g[(i, y)] < e for i in range(x + 1, mx + 1))
+    ):
+        visible.add((x, y))
+sm(len(visible))
 
 
 # part 2
-
+r = []
+for (x, y), e in g.items():
+    r.append(
+        next((i for i in range(1, y + 1) if g[(x, y - i)] >= e), y)
+        * next((i for i in range(1, my - y + 1) if g[(x, y + i)] >= e), my - y)
+        * next((i for i in range(1, x + 1) if g[(x - i, y)] >= e), x)
+        * next((i for i in range(1, mx - x + 1) if g[(x + i, y)] >= e), mx - x)
+    )
+sm(max(r))
