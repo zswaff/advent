af1db8d91dce17ef80bab807b7251a93ab56537d
Zack Swafford <zack@zswaff.com>
2022-12-19 21:39:44 -0800

    Fix typo in 2022-12-20

--- 2022/20/sln.py
+++ 2022/20/sln.py
@@ -6,31 +6,11 @@ from common import *
 
 
 class Node:
-    def __init__(self, data, old=None):
+    def __init__(self, data, old):
         self.data = data
-        self.old = data if old is None else old
+        self.old = old
         self.next = None
         self.prev = None
-
-
-def p(q):
-    vis1 = set()
-    curr = q[0]
-    while curr not in vis1:
-        vis1.add(curr)
-        curr = curr.prev
-    assert curr == q[0]
-    vis2 = set()
-    lst = []
-    curr = q[0]
-    while curr not in vis2:
-        vis2.add(curr)
-        lst.append(curr.data)
-        curr = curr.next
-    assert curr == q[0]
-    assert vis1 == vis2
-    assert len(vis1) == len(q)
-    return lst
 
 
 # part 1
@@ -65,8 +45,6 @@ sm(c)
 # part 2
 ln = len(ls) - 1
 q = [Node(e % ln, e) for e in [int(l) * 811589153 for l in ls]]
-# q = [Node((int(e) * 811589153) % ln, int(e)) for e in ls]
-# q = [Node((int(e) * 811589153) % len(ls)) for e in ls]
 for i, e in enumerate(q):
     e.next = q[(i + 1) % len(q)]
     e.prev = q[(i - 1) % len(q)]
