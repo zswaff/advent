4436aceec9ba661662b5fe1ac026cc0c0e72dc67
Zack Swafford <zack@zswaff.com>
2022-12-25 00:40:29 -0800

    Finish 2022

--- 2022/25/sln.py
+++ 2022/25/sln.py
@@ -4,5 +4,24 @@
 
 from common import *
 
+RM = {"-": -1, "=": -2}
+RMR = {v: k for k, v in RM.items()}
 
 # part 1
+c = 0
+for l in ls:
+    for i, ch in enumerate(reversed(l)):
+        c += (5**i) * int(RM.get(ch, ch))
+print(c)
+r = ""
+for i in count():
+    pw = 5**i
+    pwh = pw // 2
+    if pw - pwh <= c <= (2 * pw) + pwh:
+        break
+for j in range(i, -1, -1):
+    pw = 5**j
+    n = round(c / pw)
+    c -= n * pw
+    r += str(RMR.get(n, n))
+sm(r)
