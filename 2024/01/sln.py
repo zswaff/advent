7e639931072075b9a93d9206b757081cd3a2c1a4
Zack Swafford <zack@itsdart.com>
2024-11-30 21:06:24 -0800

    Solve 2024-12-01

--- 2024/01/sln.py
+++ 2024/01/sln.py
@@ -6,6 +6,16 @@ from common import *
 
 
 # part 1
+l1, l2 = [], []
+for e in ls:
+    a, b = pa(e, "{i}   {i}")
+    l1.append(a)
+    l2.append(b)
+l1.sort()
+l2.sort()
+sm(sum(abs(e - l2[i]) for i, e in enumerate(l1)))
 
 
 # part 2
+c = Counter(l2)
+sm(sum(e * c[e] for e in l1))
