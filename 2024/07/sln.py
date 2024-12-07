c8073db5b8a97685ee8c12b8f2d646d364bc7c4e
Zack Swafford <zack@itsdart.com>
2024-12-06 21:11:19 -0800

    Solve 2024-12-07

--- 2024/07/sln.py
+++ 2024/07/sln.py
@@ -6,6 +6,38 @@ from aoc import *
 
 
 # part 1
+def rec(vs, target):
+    if len(vs) == 1:
+        return vs[0] == target
+    if rec([vs[0] + vs[1]] + vs[2:], target):
+        return True
+    return rec([vs[0] * vs[1]] + vs[2:], target)
+
+
+c = 0
+for l in ls:
+    x, y = pa("{i}: {}", l)
+    ys = [int(e) for e in y.split(" ")]
+    if rec(ys, x):
+        c += x
+sm(c)
 
 
 # part 2
+def rec(vs, target):
+    if len(vs) == 1:
+        return vs[0] == target
+    if rec([vs[0] + vs[1]] + vs[2:], target):
+        return True
+    if rec([vs[0] * vs[1]] + vs[2:], target):
+        return True
+    return rec([int(str(vs[0]) + str(vs[1]))] + vs[2:], target)
+
+
+c = 0
+for l in ls:
+    x, y = pa("{i}: {}", l)
+    ys = [int(e) for e in y.split(" ")]
+    if rec(ys, x):
+        c += x
+sm(c)
