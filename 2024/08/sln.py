000692673b443a58d497e465016b6899c62db8fd
Zack Swafford <zack@itsdart.com>
2024-12-08 13:46:19 -0800

    Solve 2024-12-08

--- 2024/08/sln.py
+++ 2024/08/sln.py
@@ -6,6 +6,39 @@ from aoc import *
 
 
 # part 1
+g = gr()
+h = defaultdict(set)
+for k, v in g.items():
+    if v == ".":
+        continue
+    h[v].add(k)
+ans = set()
+for k, v in h.items():
+    if len(v) == 1:
+        continue
+    for (x1, y1), (x2, y2) in combinations(v, 2):
+        dx = x1 - x2
+        dy = y1 - y2
+        for nl in [(x1 + dx, y1 + dy), (x2 - dx, y2 - dy)]:
+            if nl not in g:
+                continue
+            ans.add(nl)
+sm(len(ans))
 
 
 # part 2
+ans = set()
+for k, v in h.items():
+    if len(v) == 1:
+        continue
+    for l1, l2 in combinations(v, 2):
+        (x1, y1), (x2, y2) = l1, l2
+        dx = x1 - x2
+        dy = y1 - y2
+        for it in [count(), count(-1, -1)]:
+            for i in it:
+                nl = (x1 + i * dx, y1 + i * dy)
+                if nl not in g:
+                    break
+                ans.add(nl)
+sm(len(ans))
