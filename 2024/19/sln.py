8c2f8e7192d53a70831ed5b0c8a5f7f90924437c
Zack Swafford <zack@itsdart.com>
2024-12-18 21:48:05 -0800

    Solve 2024-12-19

--- 2024/19/sln.py
+++ 2024/19/sln.py
@@ -6,6 +6,26 @@ from aoc import *
 
 
 # part 1
+os, ts = ss
+os = os[0].split(", ")
+
+
+@cache
+def r(x):
+    if len(x) == 0:
+        return True
+    return any(x.startswith(e) and r(x[len(e) :]) for e in os)
+
+
+sm(sum(r(e) for e in ts))
 
 
 # part 2
+@cache
+def r(x):
+    if len(x) == 0:
+        return 1
+    return sum(r(x[len(e) :]) for e in os if x.startswith(e))
+
+
+sm(sum(r(e) for e in ts))
