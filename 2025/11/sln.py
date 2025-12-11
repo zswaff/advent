215c0371384c729a0f9ea87c76cf244313bf4515
Zack Swafford <zack@dartai.com>
2025-12-10 22:24:41 -0800

    Solve 2025-12-11

--- 2025/11/sln.py
+++ 2025/11/sln.py
@@ -6,6 +6,26 @@ from aoc import *
 
 
 # part 1
+rv = defaultdict(set)
+for l in ls:
+    a, b = l.split(": ")
+    spl = b.split(" ")
+    for e in spl:
+        rv[e].add(a)
+
+
+@cache
+def c(g, n):
+    if n == g:
+        return 1
+    return sum(c(g, e) for e in rv[n])
+
+
+sm(c("you", "out"))
 
 
 # part 2
+sm(
+    c("svr", "dac") * c("dac", "fft") * c("fft", "out")
+    + c("svr", "fft") * c("fft", "dac") * c("dac", "out")
+)
