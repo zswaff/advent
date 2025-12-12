c11c6b5e83eec895edafa006bba10a0a9497cbf5
Zack Swafford <zack@dartai.com>
2025-12-12 13:20:56 -0800

    Solve 2025-12-12

--- 2025/12/sln.py
+++ 2025/12/sln.py
@@ -6,6 +6,13 @@ from aoc import *
 
 
 # part 1
+pas = ["".join(e).count("#") for e in ss[:-1]]
+c = 0
+for e in ss[-1]:
+    x, y, cs = pa("{i}x{i}: {}", e)
+    if sum(pas[i] * int(f) for i, f in enumerate(cs.split())) <= x * y:
+        c += 1
+sm(c)
 
 
 # part 2
